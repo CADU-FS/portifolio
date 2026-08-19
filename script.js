3b4758d52bfe7150e0064fd001e076ab5cc72ce8
Carlos Eduardo <caduclash42@gmail.com>
2026-08-19 13:53:58 -0300

    reimagined torch button

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-  // Tablet Interaction (Flashlight Button)
   const torchBtn = document.getElementById('torchBtn');
   const imageContainer = document.getElementById('dynamicImage');
 
   if (torchBtn) {
     torchBtn.addEventListener('click', () => {
-      // Toggles the 'lit-up' class that lightens or darkens the image in CSS.
       imageContainer.classList.toggle('lit-up');
+      torchBtn.classList.toggle('lit-up');
     });
   }
-
-  /*
-    window.addEventListener('mousemove', (e) => {
-      if (window.innerWidth > 1024) { 
-        let x = e.clientX;
-        let y = e.clientY;
-      }
-    });
-  */
-
 });
